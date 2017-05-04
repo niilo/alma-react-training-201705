5de0f088a36dbf0c4ddc0ed51de4b8d4b34c16cd
Niilo Ursin <niilo.ursin@gmail.com>
2017-05-04 11:36:42 +0300

    attach redux to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { compose, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './rootReducer'
 
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.css'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancers())
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById("root"));
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root')
+)
--- src/todos/components/Todos.js
+++ src/todos/components/Todos.js
@@ -1,49 +1,45 @@
-import React, { Component } from 'react';
-import InputBar from '../components/InputBar.js';
-import Todo from './Todo.js';
-import * as api from './api';
+import React, { Component } from 'react'
+import InputBar from '../../components/InputBar.js'
+import Todo from './Todo.js'
+import * as api from '../api'
 
 class Todos extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       todos: []
     }
   }
 
-  componentDidMount() {
-    this.fetchTodos();
+  componentDidMount () {
+    this.fetchTodos()
   }
 
-  fetchTodos = () =>
-    api.getTodos().then(todos => this.setState({ todos }));
+  fetchTodos = () => api.getTodos().then(todos => this.setState({ todos }))
 
-  addTodo = name =>
-    api.addTodo({name, done: false}).then(this.fetchTodos);
+  addTodo = name => api.addTodo({ name, done: false }).then(this.fetchTodos)
 
-  removeTodo = id =>
-    api.deleteTodo(id).then(this.fetchTodos);
+  removeTodo = id => api.deleteTodo(id).then(this.fetchTodos)
 
-  toggleTodo = id =>
-    api.toggleTodo(id).then(this.fetchTodos);
+  toggleTodo = id => api.toggleTodo(id).then(this.fetchTodos)
 
-  render() {
+  render () {
     return (
       <div className='form-group'>
-        <InputBar onSubmit={this.addTodo}/>
+        <InputBar onSubmit={this.addTodo} />
         <ul className='list-group'>
-          {this.state.todos.map(todo =>
+          {this.state.todos.map(todo => (
             <Todo
               key={todo.id}
               todo={todo}
               onToggleTodo={this.toggleTodo}
               onRemoveTodo={this.removeTodo}
             />
-          )}
+          ))}
         </ul>
       </div>
     )
   }
 }
 
-export default Todos;
+export default Todos
